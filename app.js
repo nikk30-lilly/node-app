a56ae03359505bf727060ead0b11b658cfca9b38
l002497 <kumar.nirmal@lilly.com>
2023-05-07 20:23:04 +0530

    checking setup v12

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD Setup!'))
+app.get('/', (req, res) => res.send('Hello CI CD Setup!'))
 app.listen(3000, () => console.log('Server ready'))
