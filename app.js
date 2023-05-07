6d70a10ef1da5bafe0fd028a52cba45e5cd767fb
l002497 <kumar.nirmal@lilly.com>
2023-05-08 00:51:29 +0530

    change yaml20

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello CI-CD V1 Setup 3!'))
+app.get('/', (req, res) => res.send('Hello CI-CD V1 Setup 4!'))
 app.listen(3000, () => console.log('Server ready'))
