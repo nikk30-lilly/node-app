bf6e05f7ce067ed4cbf23a87c6153b4622fc1d49
nikk30-lilly <kumar.nirmal@lilly.com>
2024-01-29 19:40:33 +0530

    change message@v1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello CI-CD V1 Setup 4!'))
+app.get('/', (req, res) => res.send('Hello git cheery pick'))
 app.listen(3000, () => console.log('Server ready'))
